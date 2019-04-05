f8b1b3cd01dfd771ae4df2083388b5895af627c3
Fernando <fher_17@hotmail.com>
2019-04-05 18:17:54 -0500

    Validaciones de usuario

---
+++ SpotiFake/Controllers/CancionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpotiFake.Controllers
+{
+    public class CancionController : Controller
+    {
+        // GET: Cancion
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult agregar()
+        {
+
+        }
+    }
+}
---
+++ SpotiFake/Validation/ValidacionExistente.cs
@@ -0,0 +1,27 @@
+﻿using SpotiFake.DataBase;
+using SpotiFake.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+
+namespace SpotiFake.Validation
+{
+    public class ValidacionExistente
+    {
+        SpotiFakeContext spotiFakeContext = new SpotiFakeContext();
+
+        public void existeCliente(Usuario usuario, ModelStateDictionary ModelState)
+        {
+            if (spotiFakeContext.Usuarios.Any(a => a.nombre == usuario.nombre))
+            {
+                ModelState.AddModelError("nombre", "Este nombre de usuario ya existe");
+            }
+            if (spotiFakeContext.Usuarios.Any(a => a.correoElectronico == usuario.correoElectronico))
+            {
+                ModelState.AddModelError("correoElectronico", "Este correo electrónico ya existe");
+            }
+        }
+    }
+}
