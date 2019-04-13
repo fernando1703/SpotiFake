06a9c5558ba4474daa9b4d35733f91336ab5459d
Fernando <fher_17@hotmail.com>
2019-04-13 00:09:06 -0500

    Se agregaron conroladores y vistas para registro de usuario y login

---
+++ SpotiFake/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using SpotiFake.DataBase;
+using SpotiFake.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace SpotiFake.Controllers
+{
+    public class LoginController : Controller
+    {
+        SpotiFakeContext spotiFakeContext = new SpotiFakeContext();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Ingreso(Usuario usuario)
+        {
+            Usuario usuarioConfirmado;
+            //con js ya no es importante usar el try catch 
+            try
+            {
+                usuarioConfirmado = spotiFakeContext.Usuarios.Where(o => o.correoElectronico == usuario.correoElectronico && o.contraseña == usuario.contraseña).First();
+                if (usuario.rol == "Admin")
+                {
+                    FormsAuthentication.SetAuthCookie(usuario.correoElectronico, false);
+                    return RedirectToAction("AdminIndex", "Usuario", usuarioConfirmado);
+                }
+                FormsAuthentication.SetAuthCookie(usuario.correoElectronico, false);
+                return RedirectToAction("UsuarioIndex", "Usuario", usuarioConfirmado);
+            }
+            catch (Exception)
+            {
+                return View("Index");
+            }
+
+        }
+        public ViewResult messenge()
+        {
+            return View();
+        }
+    }
+}
---
+++ SpotiFake/Controllers/UsuarioController.cs
@@ -0,0 +1,50 @@
+﻿using SpotiFake.DataBase;
+using SpotiFake.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace SpotiFake.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        SpotiFakeContext spotiFakeContext = new SpotiFakeContext();
+
+        // GET: Usuario
+        [Authorize]
+        public ActionResult UsuarioIndex(Usuario usuario)
+        {
+            return View(usuario);
+        }
+        public ActionResult logOff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Login");
+        }
+        public ActionResult GuardarUsuario(Usuario usuario)
+        {
+            if (spotiFakeContext.Usuarios.Any(o => o.correoElectronico == usuario.correoElectronico))
+                ModelState.AddModelError("Correo", "el correo ya existe!");
+            if (!ModelState.IsValid) return View("NuevoUsuario", usuario);
+            usuario.fechaCreación = DateTime.Now;
+            usuario.rol = "Usuario";
+            spotiFakeContext.Usuarios.Add(usuario);
+            spotiFakeContext.SaveChanges();
+            //ViewBag.IdUsuario = usuario.idUsuario;
+
+            return View("UsuarioIndex", usuario);
+        }
+        public ViewResult NuevoUsuario()
+        {
+            return View("NuevoUsuario", new Usuario());
+        }
+
+        public ActionResult AdminIndex(Usuario usuario)
+        {
+            return View();
+        }
+    }
+}
